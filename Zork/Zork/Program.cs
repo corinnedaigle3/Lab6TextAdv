a897838fa2f4443d1a229024b5ec9b24c9443405
corinnedaigle3 <corinnedaigle9@gmail.com>
2025-09-29 13:03:12 -0400

    test

--- Zork/Zork/Program.cs
+++ Zork/Zork/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Zork
+{
+    enum Commands
+    {
+        QUIT,
+        LOOK,
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST,
+        UNKNOWN
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Zork!");
+
+            String inputString = Console.ReadLine();
+            Commands command = ToCommand(inputString.Trim().ToUpper());
+            Console.WriteLine(command);
+        }
+
+        private static Commands ToCommand(string commandString)
+        {
+            Commands command;
+            switch (commandString)
+            {
+                case "QUIT":
+                    command = Commands.QUIT;
+                    break;
+                case "LOOK":
+                    command = Commands.LOOK;
+                    break;
+                case "NORTH":
+                    command = Commands.NORTH;
+                    break;
+                case "SOUTH":
+                    command = Commands.SOUTH;
+                    break;
+                case "EAST":
+                    command = Commands.EAST;
+                    break;
+                case "WEST":
+                    command = Commands.WEST;
+                    break;
+                default:
+                    command = Commands.UNKNOWN;
+                    break;
+
+            };
+
+            try
+                return Enum.Parse<Commands>(commandString, true);
+        }
+    }
+}
